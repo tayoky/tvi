fab7baa01cb888218ffe85b68b68d5ebfb2833b1
tayoky <tayoky4848@gmail.com>
2026-03-04 08:08:03 +0100

    renderer : sttip long filenames

--- src/render.c
+++ src/render.c
@@ -32,7 +32,15 @@ void render_status(tvi_t *tvi, win_t *win) {
 
 	const char *file = win->files[win->file_index];
 	if (!file) file = "[NO NAME]";
-	printf("%s %d,%d", file, y+1, x+1);
+
+	// see if we have enought place
+	size_t max_len = win->width - 12;
+	if (strlen(file) > max_len) {
+		file += strlen(file) - max_len;
+		printf("<%s %d,%d", file, y+1, x+1);
+	} else {
+		printf("%s %d,%d", file, y+1, x+1);
+	}
 	term_goto(win->x + win->width - 3, win->y + win->height - 1);
 	printf("tvi");
 }
